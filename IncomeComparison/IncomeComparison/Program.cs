6eb6b89d9948598b372f057c3571c0680fb89392
Ian Marshall <96323360+IMarshall@users.noreply.github.com>
2022-05-16 20:17:07 -0600

    Added IncomeComparison Project

---
+++ IncomeComparison/IncomeComparison/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncomeComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+
+            //get info for person 1 and convert inputs to appropriate data types
+            Console.WriteLine("Person 1");
+            Console.WriteLine("Hourly Rate?");
+            float hourlyRate1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Hours worked per week?");
+            int weeklyHours1 = int.Parse(Console.ReadLine());
+
+            //get info for person 2 and convert inputs to appropriate data types
+            Console.WriteLine("Person 2");
+            Console.WriteLine("Hourly Rate?");
+            float hourlyRate2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Hours worked per week?");
+            int weeklyHours2 = int.Parse(Console.ReadLine());
+
+            //calculate annual salaries and compare them
+            float annualSalary1 = hourlyRate1 * weeklyHours1 * 52;
+            float annualSalary2 = hourlyRate2 * weeklyHours2 * 52;
+            bool comparison = annualSalary1 > annualSalary2;
+
+            //output results to user
+            Console.WriteLine("Annual salary of Person 1:");
+            Console.WriteLine(annualSalary1);
+            Console.WriteLine("Annual salary of Person 2:");
+            Console.WriteLine(annualSalary2);
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            Console.WriteLine(comparison);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NumberOperations/NumberOperations/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string prompt = "Enter a number:";
+            int userNum;
+            int result;
+
+            Console.WriteLine(prompt);
+            userNum = int.Parse(Console.ReadLine());
+            result = userNum * 50;
+            Console.WriteLine(userNum + " * 50 = " + result);
+
+            Console.WriteLine(prompt);
+            userNum = int.Parse(Console.ReadLine());
+            result = userNum + 25;
+            Console.WriteLine(userNum + " + 25 = " + result);
+
+            Console.WriteLine(prompt);
+            userNum = int.Parse(Console.ReadLine());
+            float quotient = userNum / 12.5f;
+            Console.WriteLine(userNum + " / 12.5 = " + quotient);
+
+            Console.WriteLine(prompt);
+            userNum = int.Parse(Console.ReadLine());
+            bool greater = userNum > 50;
+            Console.WriteLine("Is your number greater than 50? - " + greater);
+
+            Console.WriteLine(prompt);
+            userNum = int.Parse(Console.ReadLine());
+            result = userNum % 7;
+            Console.WriteLine("The remainder of " + userNum + "/7 is " + result);
+
+            Console.ReadLine();
+        }
+    }
+}
