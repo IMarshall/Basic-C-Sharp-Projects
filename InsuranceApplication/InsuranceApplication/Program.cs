35391c3142761cc61c228aa608181dfb0ac88b99
Ian Marshall <96323360+IMarshall@users.noreply.github.com>
2022-05-21 18:32:20 -0600

    Added insurance app

---
+++ InsuranceApplication/InsuranceApplication/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask for info and store it in the appropriate variables
+            Console.WriteLine("What is your age?");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? answer \"true\" or \"false\"");
+            bool dui = bool.Parse(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = int.Parse(Console.ReadLine());
+            Console.WriteLine("Qualified?");
+
+            // Use && operator to determine if the applicant is qualified
+            Console.WriteLine(age > 15 && dui == false && tickets <= 3);
+
+            Console.ReadLine();
+        }
+    }
+}
