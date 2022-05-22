f296c73a46615d6623f0d4cc1721a74765d4cd6b
Ian Marshall <96323360+IMarshall@users.noreply.github.com>
2022-05-21 19:19:39 -0600

    Added ShippingQuote Project

---
+++ ShippingQuote/ShippingQuote/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShippingQuote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Start message
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            
+            // Ask user for weight
+            Console.WriteLine("How much does your package weigh?");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            // Evaluate if package is under weight limit.
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+            }
+            else
+            {
+                // Ask for package dimensions
+                Console.WriteLine("What is the package width?");
+                int width = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("What is the package height?");
+                int height = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("What is the package length?");
+                int length = Convert.ToInt32(Console.ReadLine());
+
+                // Evaluate if package is under size limit
+                if (width + height + length > 50)
+                {
+                    Console.WriteLine("Package too large to be shipped via Package Express. Have a good day.");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    // Calculate quote price and convert to float
+                    int quotePriceInt = ((width * height * length) * weight);
+                    float quotePrice = (float)quotePriceInt / 100;
+
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + quotePrice);
+                    Console.WriteLine("Thank you!");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ ShippingQuote/ShippingQuote/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ShippingQuote")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("ShippingQuote")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("eb8282a0-b9e5-499c-94cd-55f5de7b848e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
