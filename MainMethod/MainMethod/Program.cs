c07d1726be3d24978fc54a80cd9f6bb9799e59cc
Ian Marshall <96323360+IMarshall@users.noreply.github.com>
2022-06-11 16:43:33 -0600

    Added MainMethod Assignment

---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the Operation class
+            Operation o = new Operation();
+
+            //call class methods and assign the returned values to variables
+            int num1 = o.MultiplyByTwo(5);
+            Console.WriteLine(num1);
+
+            int num2 = o.DivideByOne(3.14F);
+            Console.WriteLine(num2);
+
+            int num3 = o.AddFive("64");
+            Console.WriteLine(num3);
+
+            Console.ReadLine();
+        }
+    }
+}
