212134c2aa3edc791add30b7a4b2c622b8f4a5e1
Ian Marshall <96323360+IMarshall@users.noreply.github.com>
2022-06-11 14:45:21 -0600

    Added TwentyOne Project

---
+++ TwentyOne/TwentyOne/Card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Card
+    {
+        public Card()
+        {
+            
+        }
+        public string Suit { get; set; }
+        public string Face { get; set; }
+    }
+}
