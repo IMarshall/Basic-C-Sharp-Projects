05997da4003aa185363239ddd2c19190bc3ce859
Ian Marshall <96323360+IMarshall@users.noreply.github.com>
2022-06-15 17:53:48 -0600

    Added VoidMethod Assignment

---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the class
+            Operation UserOperation = new Operation();
+
+            //call method with two parameters
+            UserOperation.Squared(12, 42);
+
+            //call method with named parameters
+            UserOperation.Squared(b: 54, a: 14);
+
+            Console.ReadLine();
+        }
+    }
+}
