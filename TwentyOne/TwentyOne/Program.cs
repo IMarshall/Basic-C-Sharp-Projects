86638de9ff694ed3452a076d77293c584a806085
Ian Marshall <96323360+IMarshall@users.noreply.github.com>
2022-06-18 17:21:43 -0600

    Added Generics Assignment

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,13 +10,7 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            Game game = new TwentyOneGame();
-            game.Players = new List<Player>();
-            Player player = new Player();
-            player.Name = "Ian";
-            game += player;
-            game -= player;
-
+            
             Deck deck = new Deck();
             deck.Shuffle(3);
             
