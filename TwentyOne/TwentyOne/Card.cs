973133629697ca7ffcd160e3d44e89498679e2d8
Ian Marshall <96323360+IMarshall@users.noreply.github.com>
2022-06-18 18:11:34 -0600

    Added ParsingEnums Assignment

--- TwentyOne/TwentyOne/Card.cs
+++ TwentyOne/TwentyOne/Card.cs
@@ -12,7 +12,31 @@ namespace TwentyOne
         {
             
         }
-        public string Suit { get; set; }
-        public string Face { get; set; }
+        public Suit Suit { get; set; }
+        public Face Face { get; set; }
+    }
+    public enum Suit
+    {
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
+    }
+
+    public enum Face
+    {
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
     }
 }
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,14 +10,14 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            
             Deck deck = new Deck();
             deck.Shuffle(3);
-            
+
             foreach (Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
+
             Console.WriteLine(deck.Cards.Count);
             Console.ReadLine();
         }
