e2d00dd99343733107c657f795cca5afae20c3a5
Ian Marshall <96323360+IMarshall@users.noreply.github.com>
2022-06-23 19:35:58 -0600

    Added DateTimeNow Assignment

---
+++ DateTimeNow/DateTimeNow/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeNow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DEFINE DATETIME WITH CURRENT DATE AND TIME
+            DateTime now = DateTime.Now;
+            Console.WriteLine("The current date and time is {0}\n", now);
+
+            Console.WriteLine("Please enter a number.");
+            //ASSIGN USER INPUT TO DOUBLE
+            double x = Convert.ToInt32(Console.ReadLine());
+            //DEFINE ANOTHER DATETIME THAT CALCULATES NOW+X HOURS USING THE .AddHours() METHOD
+            DateTime later = now.AddHours(x);
+
+            //PRINT RESULTS TO CONSOLE
+            Console.WriteLine("\n{0} hours from now, the date and time will be {1}", x, later);
+
+            Console.Read();
+        }
+    }
+}
--- TwentyOne/TwentyOne/Dealer.cs
+++ TwentyOne/TwentyOne/Dealer.cs
@@ -20,6 +20,7 @@ namespace TwentyOne
             Console.WriteLine(card);
             using (StreamWriter file = new StreamWriter(@"C:\Users\imars\OneDrive\Documents\GitHub\Basic-C-Sharp-Projects\TwentyOne\log.txt", true))
             {
+                file.WriteLine(DateTime.Now);
                 file.WriteLine(card);
             }
             Deck.Cards.RemoveAt(0);
