b059cc992b4b6eb8722da6798bdf5e50d7e7ed73
Ian Marshall <96323360+IMarshall@users.noreply.github.com>
2022-07-04 18:05:35 -0600

    Added CodeFirstDb Project

---
+++ CodeFirstDb/CodeFirstDb/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeFirstDb
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Tony Soprano" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
---
+++ CodeFirstDb/CodeFirstDb/SchoolContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace CodeFirstDb
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
